3314bed25422c7c629f074dc23b5d4471ad07e7e
Thiago Santos Borges <tsb.snf16@uea.edu.br>
2021-10-06 23:36:25 -0400

    Script da camera

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace Camera
@@ -5,11 +6,24 @@ namespace Camera
     public class CameraFollow : MonoBehaviour
     {
         [SerializeField] private Transform target;
-        [SerializeField] private Vector3 offset;
+        [SerializeField] private float speed = 3f;
+        [SerializeField] private float distance = 0.5f;
 
-        private void Update()
+        private void Start()
         {
-            transform.position = target.position + offset;
+            transform.rotation = Quaternion.Euler(45, 0, 0);
+        }
+
+        private void FixedUpdate()
+        {
+            var time = speed * Time.fixedDeltaTime;
+            var position = transform.position;
+
+            position.x = Mathf.Lerp(transform.position.x, target.position.x, time);
+            position.y = Mathf.Lerp(transform.position.y + distance, target.position.y, time);
+            position.z = Mathf.Lerp(transform.position.z - distance, target.position.z, time);
+            
+            transform.position = position;
         }
     }
 }
